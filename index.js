0242ce0e9f185211a0e6ae6d2b1b0eca6f65a788
Rey Serrano <serranoj.rreynaldo@gmail.com>
2021-09-26 14:18:43 -0500

    Created an array of objects for most of the items needed per the assignment readme. Also converted the file into a md file instead of it being created as a JS

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your project title?',
+    },
+    {
+      type: 'input',
+      message: 'Brief description of your project.',
+      name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Explain what steps are necessary to use the project.',
+        name: 'steps',
+    },
+    {
+        type: 'input',
+        message: 'List the name of collaborators. If just you, just put your name.',
+        name: 'collaborators',
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.title.toLowerCase().split(' ').join('')}`;
 
+    fs.writeFile(filename+'.md', JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Readme Generated!')
+    );
+  });
